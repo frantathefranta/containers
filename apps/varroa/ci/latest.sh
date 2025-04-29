5df379720fee34ccb5819ed63270e8a188d2e521
Franta <64412753+frantathefranta@users.noreply.github.com>
2025-04-28 22:26:09 -0400

    Update latest.sh

--- apps/varroa/ci/latest.sh
+++ apps/varroa/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-version="$(curl -sX GET "https://gitlab.com/passelecasque/varroa/-/releases/permalink/latest" | sed -n 's|.*releases/\([^"]*\).*|\1|p')"
+version="$(curl -s "https://gitlab.com/api/v4/projects/passelecasque%2Fvarroa/releases" | jq -r '.[0].name')"
 # Gitlab doesn't have an API for checking a latest release. This curl query gets me the least amount of output, that looks like this:
 # <html><body>You are being <a href="https://gitlab.com/passelecasque/varroa/-/releases/v35">redirected</a>.</body></html>%
 #
